7d4b17297eb5e7d95afa7f44db6132f3c710db72
Baba-ott <inig0001@hz.nl>
2023-06-21 14:21:31 +0200

    initial commit

---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+
+    public function someMethod()
+    {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+
+        // the rest of the controller method
+    }
+
+}
