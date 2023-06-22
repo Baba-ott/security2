a557a2d3db3693f04d406793f80d4413773b84d7
Baba-ott <inig0001@hz.nl>
2023-06-22 17:00:35 +0200

    Jawsdb_url

--- config/database.php
+++ config/database.php
@@ -68,12 +68,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => $url,
+            'host' => $DATABASE_URL['host'] ?? null,
+            'port' => $DATABASE_URL['port'] ?? null,
+            'database' => isset($DATABASE_URL['path']) ? ltrim($DATABASE_URL['path'], '/') : null,
+            'username' => $DATABASE_URL['user'] ?? null,
+            'password' => $DATABASE_URL['pass'] ?? null,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
