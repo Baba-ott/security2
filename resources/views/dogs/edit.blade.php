6efbdc4efe6fde66a1fe3953d559ee9c62c388be
Baba-ott <inig0001@hz.nl>
2023-06-22 23:40:32 +0200

    crud improved.2

--- resources/views/dogs/edit.blade.php
+++ resources/views/dogs/edit.blade.php
@@ -1,41 +1,60 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dogs</title>
+    <!-- Add Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+<div class="container py-5">
+    <h1 class="text-center mb-5">Dogs</h1>
+    <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10">
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <a class="btn btn-primary" href="/dashboard">Dashboard</a>
+                </div>
+            </div>
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <a class="btn btn-success" href="/dogs/create">Create a Dog</a>
+                </div>
+            </div>
+            <div class="card shadow-sm my-5">
+                <div class="card-header bg-light">
+                    <h3>Edit Dog</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('dogs.update', $dog->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>Edit Dog</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="{{ route('dogs.update', $dog->id) }}" method="POST">
-                            @csrf
-                            @method('PUT')
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" id="name" class="form-control" value="{{ $dog->name }}">
+                        </div>
 
-                            <div class="form-group">
-                                <label for="name">Name:</label>
-                                <input type="text" name="name" id="name" class="form-control" value="{{ $dog->name }}">
-                            </div>
+                        <div class="form-group">
+                            <label for="age">Age:</label>
+                            <input type="number" name="age" id="age" class="form-control" value="{{ $dog->age }}">
+                        </div>
 
-                            <div class="form-group">
-                                <label for="age">Age:</label>
-                                <input type="number" name="age" id="age" class="form-control" value="{{ $dog->age }}">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="breed">Breed:</label>
-                                <input type="text" name="breed" id="breed" class="form-control" value="{{ $dog->breed }}">
-                            </div>
+                        <div class="form-group">
+                            <label for="breed">Breed:</label>
+                            <input type="text" name="breed" id="breed" class="form-control" value="{{ $dog->breed }}">
+                        </div>
 
 
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Update Dog</button>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Update Dog</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</div>
+<!-- Add Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+</body>
+</html>
--- resources/views/dogs/show.blade.php
+++ resources/views/dogs/show.blade.php
@@ -1,33 +1,51 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-
-                        <h3>Name: {{ $dog->name }}</h3>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dogs</title>
+    <!-- Add Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+<div class="container py-5">
+    <h1 class="text-center mb-5">Dogs</h1>
+    <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10">
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <a class="btn btn-primary" href="/dashboard">Dashboard</a>
+                </div>
+            </div>
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <a class="btn btn-success" href="/dogs/create">Create a Dog</a>
+                </div>
+            </div>
+            <div class="card shadow-sm my-5">
+                <div class="card-header bg-light">
+                    <h3>Name: {{ $dog->name }}</h3>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <strong>Age:</strong> {{ $dog->age }}
                     </div>
-                    <div class="card-body">
-                        <div class="mb-3">
-                            <strong>Age:</strong> {{ $dog->age }}
-                        </div>
-                        <div class="mb-3">
-                            <strong>Breed:</strong> {{ $dog->breed }}
-                        </div>
+                    <div class="mb-3">
+                        <strong>Breed:</strong> {{ $dog->breed }}
                     </div>
-                    <div class="card-footer d-flex justify-content-center">
-                        <a href="{{ route('dogs.edit', $dog->id) }}" class="btn btn-primary mr-2">Edit Dog</a>
-                        <a href="{{ route('dogs.index') }}" class="btn btn-secondary mr-2">Back</a>
-                        <form action="{{ route('dogs.destroy', $dog->id) }}" method="POST" style="display: inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this dog?')">Delete Dog</button>
-                        </form>
-                    </div>
+                </div>
+                <div class="card-footer d-flex justify-content-center">
+                    <a href="{{ route('dogs.edit', $dog->id) }}" class="btn btn-primary mr-2">Edit Dog</a>
+                    <a href="{{ route('dogs.index') }}" class="btn btn-secondary mr-2">Back</a>
+                    <form action="{{ route('dogs.destroy', $dog->id) }}" method="POST" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this dog?')">Delete Dog</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</div>
+<!-- Add Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+</body>
+</html>
